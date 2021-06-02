04396f69166c83b157936fcaf100c7c99dc6864e
Bekim Demiroski <bekimd@microsoft.com>
2021-06-02 08:41:09 +0800

    fixes to support windows vm

--- CreateVM.ps1
+++ CreateVM.ps1
@@ -2,32 +2,40 @@
 Param(
     [string]$ResourceGroupName,
     [string]$VMName,
+    [string]$VMSize = "Standard_D2s_v3",
     [string]$OSType, 
     [string]$AdminUserName, 
-    [string]$KeyFile, 
+    [string]$AdminSecret, 
     [string]$site = $env:AZURESITE
   )
 
-$template = "templates\createvm.json"
+$templateFile = "templates\createvm.json"
+
+$template = get-content ($templateFile) -raw | ConvertFrom-Json -Depth 32
 
 if ( $OsType -eq 'linux' ) {
   $imagePublisher =  "Canonical"
   $imageOffer = "UbuntuServer"
   $imageSku = "18.04-LTS"
   $imageVersion = "latest"
+  $adminSecretData = Get-Content -Path $Secret
+  $template.resources[4].properties.osProfile.PSObject.properties.remove('adminPassword')
 } 
 else {
   $imagePublisher =  "MicrosoftWindowsServer"
   $imageOffer = "WindowsServer"
   $imageSku = "2016-Datacenter"
   $imageVersion = "latest"  
+  $adminSecretData = $AdminSecret
+  $template.resources[4].properties.osProfile.PSObject.properties.remove('linuxConfiguration')
 }
 
-$keyValue = Get-Content -Path $KeyFile
+$templateFile = ".\temp.json"
+$template | ConvertTo-Json -depth 32 | set-content ( $templateFile )
 
 Write-Host "Creating new VM..."
 
-New-AzResourceGroupDeployment -verbose -name $ResourceGroupName -resourcegroupname $ResourceGroupName -templatefile $template `
+New-AzResourceGroupDeployment -verbose -name $ResourceGroupName -resourcegroupname $ResourceGroupName -templatefile $templateFile `
                               -vmName $VMName `
                               -imagePublisher $imagePublisher `
                               -imageOffer $imageOffer `
@@ -35,7 +43,7 @@ New-AzResourceGroupDeployment -verbose -name $ResourceGroupName -resourcegroupna
                               -imageVersion $imageVersion `
                               -dnsLabelPrefix $VMName `
                               -adminUserName $AdminUserName `
-                              -adminPublicKey $keyValue `
+                              -adminSecret  $adminSecretData `
                               -site $site
 
 
